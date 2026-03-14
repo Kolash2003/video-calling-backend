b70bd251b2bcae67bed318c42d2e3234fce12f81
Aneesh Kolar <kolaraneesh2003@gmail.com>
2026-03-14 10:47:40 +0530

    Added basic express setup

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import http from "http";
+import serverConfig from "./config/serverConfig";
+import { Server } from "socket.io";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    })
+})
+
+server.listen(serverConfig.PORT, () => {
+    console.log(`Server running on port ${serverConfig.PORT}`);
+});
